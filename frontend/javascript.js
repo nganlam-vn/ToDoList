ae753a3da7bc7eae6f96b11afb4b96b9e9dc7bfb
Ngan ne <watanabilinlin@gmail.com>
2024-02-05 14:12:10 +0700

    add js file

--- frontend/javascript.js
+++ frontend/javascript.js
@@ -1,16 +1,41 @@
+// document.querySelector('#push').onclick = function(){
+//     if(document.querySelector('#newtask input').value.length == 0){
+//         alert("You haven't entered your task name!");
+//     }
+//     else{ 
+//         // innerHTML to display tasks onscreen
+//         var myBtn = document.getElementById("push")
+//         const taskAdd= () =>{
+//             var task = document.getElementById("tasks");
+//             var newTask = document.createElement("p")
+//             newTask.textContent = inp.value;
+//             task.appendChild(newTask);                                                                                                                            
+//         }
+//         myBtn.onclick=taskAdd
+//     }
+// }
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
-        alert("You haven't entered your task name!");
+        alert("Kindly Enter Task Name!!!!")
     }
-    else{ 
-        // innerHTML to display tasks onscreen
-        var myBtn = document.getElementById("push")
-        const taskAdd= () =>{
-            var task = document.getElementById("tasks");
-            var newTask = document.createElement("p")
-            newTask.textContent = inp.value;
-            task.appendChild(newTask);                                                                                                                            
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
         }
-        myBtn.onclick=taskAdd
     }
 }
